05ea935bdac19415db98187d36cce5d176507a3a
Jason Whitted <jason.whitted@clover.com>
2020-05-11 10:02:07 -0600

    Revert to kotlinx-coroutines 1.3.5 (due to issue #2004) and bump to 0.0.5

--- src/main/kotlin/com/github/etalisoft/kotlin/redux/definitions.kt
+++
@@ -1,4 +0,0 @@
-package com.github.etalisoft.kotlin.redux
-
-typealias Thunk<T> = suspend (dispatch: Dispatch, getState: GetState<T>) -> Any
-typealias ThunkExtra<T> = suspend (dispatch: Dispatch, getState: GetState<T>, extraArgument: Any?) -> Any
--- src/main/kotlin/com/github/etalisoft/kotlin/redux/thunk.kt
+++ src/main/kotlin/com/github/etalisoft/kotlin/redux/thunk.kt
@@ -1,5 +1,8 @@
 package com.github.etalisoft.kotlin.redux
 
+typealias Thunk<T> = suspend (dispatch: Dispatch, getState: GetState<T>) -> Any
+typealias ThunkExtra<T> = suspend (dispatch: Dispatch, getState: GetState<T>, extraArgument: Any?) -> Any
+
 fun <State> thunk(block: Thunk<State>) = block
 
 fun <State> thunk(block: ThunkExtra<State>) = block
